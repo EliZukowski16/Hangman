2680180545114a5ab01617b1605dcd25e1b0baa7
Eli Zukowski <eli.zukowski@gmail.com>
2016-09-13 16:44:18 -0400

    using local storage

--- hangman.js
+++ hangman.js
@@ -2,71 +2,91 @@
  * 
  */
 
-var wordList = ["pirate", "banana", "elephant", "abracadabra"];
+var wordList = [ "pirate", "banana", "elephant", "abracadabra" ];
 
 var word;
-var wordInPlay = [];
-for (i = 0; i < word.length; i++) {
-	wordInPlay.push("_");
-}
-var guessesMade = [];
-var guessesLeft = 6;
+var wordInPlay;
+var guessesMade;
+var guessesLeft;
 
 $(document).ready(function() {
-	if(word == null) {
-		word = localStorage.getItem("word");
-	}
-	
-	
-	
 	var checkButton = $("#checkGuess");
 	var newGameButton = $("#newGame");
 	var userGuess = $("#userGuess");
-	var guess = userGuess.val();
+	var guessed = $("#alreadyGuessed");
 	var left = $("#guessesLeft");
 	var made = $("#guessesMade");
 	var game = $("#wordInPlay");
+	var gameStatus = $("#gameStatus");
 
-	left.text(guessesLeft);
-	made.text(guessesMade);
-	game.text(wordInPlay.join(""));
+	var guess = userGuess.val();
+	var won = false;
+	var lost = false;
 
-	newGameButton.click(function() {
-		
-		
-		
-	})
-	
+	function saveGame() {
+		localStorage.setItem("word", (word));
+		localStorage.setItem("wordInPlay", JSON.stringify(wordInPlay))
+		localStorage.setItem("guessesMade", JSON.stringify(guessesMade))
+		localStorage.setItem("guessesLeft", (guessesLeft))
+		localStorage.setItem("gameSaved", "yes");
+	}
 
-	checkButton.click(function() {
+	function loadGame() {
+		word = (localStorage.getItem("word"))
+		wordInPlay = JSON.parse(localStorage.getItem("wordInPlay"))
+		guessesMade = JSON.parse(localStorage.getItem("guessesMade"))
+		guessesLeft = (localStorage.getItem("guessesLeft"))
+	}
 
-		guess = userGuess.val().slice(0, 1);
-
-		if (alreadyGuessed(guess)) {
-			alert(guess + " has already been guessed");
-		} else {
-			if (!checkGuess(guess)) {
-				guessesLeft -= 1;
-			}
-		}
+	if (localStorage.getItem("gameSaved") != "yes") {
+		console.log("new game");
+		newGame();
+	} else {
+		loadGame();
 
 		left.text(guessesLeft);
 		made.text(guessesMade);
 		game.text(wordInPlay.join(""));
+	}
 
+	newGameButton.click(function() {
+		newGame();
+	})
+
+	checkButton.click(function() {
+		if (!Boolean(won) && !Boolean(lost)) {
+			guessed.hide();
+
+			guess = userGuess.val().slice(0, 1);
+
+			if (alreadyGuessed(guess)) {
+				guessed.text(guess + " has already been guessed");
+				guessed.show();
+			} else if (!checkGuess(guess)) {
+				guessesLeft -= 1;
+			}
+
+
+			userGuess.val("");
+			left.text(guessesLeft);
+			made.text(guessesMade);
+			game.text(wordInPlay.join(""));
+			saveGame();
+			checkGame();
+		}
 	})
 
 
 	function alreadyGuessed(guess) {
-		var guessed = false;
+		var isGuessed = false;
 
 		for (i = 0; i < guessesMade.length; i++) {
 			if (guess.toLowerCase() == guessesMade[i].toLowerCase()) {
-				guessed = true;
+				isGuessed = true;
 			}
 		}
 
-		return guessed;
+		return isGuessed;
 	}
 
 	function checkGuess(guess) {
@@ -82,5 +102,56 @@ $(document).ready(function() {
 		}
 
 		return guessInWord;
+	}
+
+	function newGame() {
+		won = false;
+		lost = false;
+		gameStatus.hide();
+		word = wordList[Math.floor(Math.random() * wordList.length)];
+		console.log(word)
+		localStorage.setItem("word", word)
+		wordInPlay = [];
+		for (i = 0; i < word.length; i++) {
+			wordInPlay.push("_");
+		}
+		guessesMade = [];
+		guessesLeft = 6;
+
+		saveGame();
+
+		left.text(guessesLeft);
+		made.text(guessesMade);
+		game.text(wordInPlay.join(""));
+	}
+
+	function checkGame() {
+		checkWon();
+		checkLost();
+	}
+
+	function checkWon() {
+		won = true;
+		for (i = 0; i < word.length; i++) {
+			if (word[i] != wordInPlay[i]) {
+				won = false;
+			}
+		}
+
+		if (won == true) {
+			gameStatus.text("You Won!");
+			gameStatus.show();
+		}
+	}
+
+	function checkLost() {
+		if (guessesLeft <= 0) {
+			lost = true;
+		}
+
+		if (lost == true) {
+			gameStatus.text("You Lost!");
+			gameStatus.show();
+		}
 	}
 })
